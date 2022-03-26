20afd0385b5e078225ac3a13741ede94f777c938
Madeline Morris <madehopemorr@gmail.com>
2022-03-26 13:25:43 -0700

    index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+var el = document.querySelector("#page");
+var link = el.querySelector("img");
+
+function setColor() {var input = document.querySelectorAll("input") [0];
+el.style.color = input.value;
+}
+
+function setLink() {
+    var input = document.querySelectorAll("input") [1];
+    link.setAttribute("src", input.value);
+}
+
+function toggleUnderline() {
+    el.classList.toggle("underline");
+}
+
+function toggleItalic() {
+    el.classList.toggle("italic");
+}
+
+function toggleBold() {
+    el.classList.toggle("bold");
+}
+
+function addMonospace() {
+    el.classList.add("gray-text", "mono");
+}
+
+function removeFormat() {
+    el.classList.remove("underline", "italic", "bold", "gray-text", "mono");
+}
+
